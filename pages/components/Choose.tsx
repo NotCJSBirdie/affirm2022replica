c776892825a8a4ae985b4dde8e425449729e4396
Carl Serquiña <serquinajhunlo@gmail.com>
2022-02-01 05:47:51 +0800

    morechooserevisions

--- pages/components/Choose.tsx
+++ pages/components/Choose.tsx
@@ -7,7 +7,7 @@ const Choose = () => {
     <div className="py-24  px-4 md:px-12">
       <div className="grid grid-cols-1 grid-rows-9 md:grid-cols-4 md:grid-rows-3 gap-2">
         <div className="col-span-1 row-span-1 md:col-span-2 md:row-span-2 p-4 flex flex-col items-start">
-          <div className=" w-full h-full bg-blue-400 pt-80">
+          <div id={styles.walmart} className=" w-full h-full pt-80">
             <img
               src="https://cdn-assets.affirm.com/vcn_buy/v1/merchants/X96ZPHNIR5GRHG1W/logo2019111308/logo_offer.png?width=128px&format=jpg&quality=75"
               alt="wallmartlogo"
@@ -17,14 +17,17 @@ const Choose = () => {
 
           <h1 className="my-2">Wallmart.com</h1>
         </div>
+
         <div className="col-span-1 row-span-1 p-4 flex flex-col items-start">
           <img
             src="https://cdn-assets.affirm.com/merchant/promos/8L2VTJ7XV2QQ4PCU/hero2022010635/hero_3.png?width=576px&format=jpg&quality=75"
             alt="walmart"
             className="w-full h-full"
           />
+
           <h1 className="my-2">Peloton E-commerce</h1>
         </div>
+
         <div className="col-span-1 row-span-1 p-4 flex flex-col items-start">
           <img
             src="https://cdn-assets.affirm.com/merchant/promos/N47TOZUXEXW2S0N1/hero2021041337/amazonhero.png?width=576px&format=jpg&quality=75"
@@ -33,6 +36,7 @@ const Choose = () => {
           />
           <h1 className="my-2">Amazon</h1>
         </div>
+
         <div className="col-span-1 row-span-1 p-4 flex flex-col items-start">
           <img
             src="https://cdn-assets.affirm.com/vcn_buy/v1/merchants/Z0BQ6KB8BLMXH75Q/hero20191204/hero_offer.png?width=576px&format=jpg&quality=75"
@@ -41,6 +45,7 @@ const Choose = () => {
           />
           <h1 className="my-2">Expedia Hotels & Vacation Packages</h1>
         </div>
+
         <div className="col-span-1 row-span-1 p-4 flex flex-col items-start">
           <img
             src="https://cdn-assets.affirm.com/merchant/promos/NJUT88CFF7H7SR5W/hero2021071650/hero_10.png?width=576px&format=jpg&quality=75"
