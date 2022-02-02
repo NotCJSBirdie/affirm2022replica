cc58a33ae7be0e77e6513e0e1854cdefdf2cb37a
Carl Serquiña <serquinajhunlo@gmail.com>
2022-02-03 06:08:38 +0800

    howtobuyrevisions

--- pages/components/HowToBuy.tsx
+++ pages/components/HowToBuy.tsx
@@ -1,12 +1,13 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
+import styles from "../components/css/HowToBuy.module.css";
 
 const HowToBuy = () => {
   return (
     <div>
       <Carousel showThumbs={false}>
         <div className="">
-          <p className="p-32 py-72 bg-blue-400">
+          <p id={styles.slide1} className="p-32 py-72">
             <section className="text-gray-600 body-font">
               <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
@@ -38,6 +39,10 @@ const HowToBuy = () => {
                       Learn More
                     </button>
                   </div>
+
+                  <div className="flex flex-row mt-5">
+                    This eligibility check won't affect your credit score.
+                  </div>
                 </div>
 
                 <div className="w-full md:w-1/2">
@@ -52,31 +57,45 @@ const HowToBuy = () => {
           </p>
         </div>
         <div>
-          <p className="p-32 py-72 bg-red-400">
+          <p id={styles.slide1} className="p-32 py-72">
             <section className="text-gray-600 body-font">
               <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                  <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-                    Before they sold out
-                    <br className="hidden lg:inline-block" />
-                    readymade gluten
+                  <h1 className="title-font sm:text-5xl text-3xl mb-4 font-medium text-gray-900">
+                    How to buy with Affirm
                   </h1>
+
+                  <div className="flex flex-row items-center">
+                    <h1 className="mr-5">Step 1</h1>
+                    <h1 className="mr-5">Step 2</h1>
+                    <h1 className="mr-5">Step 3</h1>
+                  </div>
+
+                  <h1 className="title-font sm:text-5xl text-3xl mb-4 font-medium text-gray-900">
+                    Choose your payment terms
+                  </h1>
+
                   <p className="mb-8 leading-relaxed">
-                    Copper mug try-hard pitchfork pour-over freegan heirloom
-                    neutra air plant cold-pressed tacos poke beard tote bag.
-                    Heirloom echo park mlkshk tote bag selvage hot chicken
-                    authentic tumeric truffaut hexagon try-hard chambray.
+                    You’re in control. Pick the payment option that works for
+                    you and your budget—from 4 interest-free payments every 2
+                    weeks to monthly installments.
                   </p>
+
                   <div className="flex justify-center">
                     <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                      Button
+                      Get Prequalified
                     </button>
                     <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
-                      Button
+                      Learn More
                     </button>
                   </div>
+
+                  <div className="flex flex-row mt-5">
+                    This eligibility check won't affect your credit score.
+                  </div>
                 </div>
-                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+
+                <div className="w-full md:w-1/2">
                   <img
                     className="object-cover object-center rounded"
                     alt="hero"
@@ -88,31 +107,45 @@ const HowToBuy = () => {
           </p>
         </div>
         <div>
-          <p className="p-32 py-72 bg-green-400">
+          <p id={styles.slide1} className="p-32 py-72">
             <section className="text-gray-600 body-font">
               <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                  <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-                    Before they sold out
-                    <br className="hidden lg:inline-block" />
-                    readymade gluten
+                  <h1 className="title-font sm:text-5xl text-3xl mb-4 font-medium text-gray-900">
+                    How to buy with Affirm
                   </h1>
+
+                  <div className="flex flex-row items-center">
+                    <h1 className="mr-5">Step 1</h1>
+                    <h1 className="mr-5">Step 2</h1>
+                    <h1 className="mr-5">Step 3</h1>
+                  </div>
+
+                  <h1 className="title-font sm:text-5xl text-3xl mb-4 font-medium text-gray-900">
+                    Go shopping
+                  </h1>
+
                   <p className="mb-8 leading-relaxed">
-                    Copper mug try-hard pitchfork pour-over freegan heirloom
-                    neutra air plant cold-pressed tacos poke beard tote bag.
-                    Heirloom echo park mlkshk tote bag selvage hot chicken
-                    authentic tumeric truffaut hexagon try-hard chambray.
+                    Shop your favorite stores online or in-store and pay later
+                    with Affirm. You’ll see us at checkout, or you can request a
+                    virtual card in the Affirm app.
                   </p>
+
                   <div className="flex justify-center">
                     <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                      Button
+                      Get Prequalified
                     </button>
                     <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
-                      Button
+                      Learn More
                     </button>
                   </div>
+
+                  <div className="flex flex-row mt-5">
+                    This eligibility check won't affect your credit score.
+                  </div>
                 </div>
-                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+
+                <div className="w-full md:w-1/2 ">
                   <img
                     className="object-cover object-center rounded"
                     alt="hero"
