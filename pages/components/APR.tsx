931105500842704c7868900d80478bb0cca7e3cc
Carl Serquiña <serquinajhunlo@gmail.com>
2022-02-04 05:46:31 +0800

    moreedits

--- pages/components/APR.tsx
+++ pages/components/APR.tsx
@@ -24,7 +24,9 @@ const APR = () => {
             />
           </div>
 
-          <h1 className="my-2">Peloton E-commerce</h1>
+          <h1 className="mt-2">Simply Mac</h1>
+
+          <h1 className="mb-2">As low as 0% APR</h1>
         </div>
 
         <div className="col-span-1 row-span-1 p-4 flex flex-col items-start">
@@ -38,91 +40,105 @@ const APR = () => {
             />
           </div>
 
-          <h1 className="my-2">Peloton E-commerce</h1>
+          <h1 className="mt-2">Room & Board</h1>
+
+          <h1 className="mb-2">As low as 0% APR</h1>
         </div>
 
         <div className="col-span-1 row-span-1 p-4 flex flex-col items-start">
           <div
-            id={styles.roomandboard}
+            id={styles.casper}
             className=" w-full h-full pt-48 md:pt-12 ease-in-out rounded-3xl"
           >
             <img
-              src="https://cdn-assets.affirm.com/vcn_buy/v1/merchants/NTMW6QA9FVTCHPFW/logo_20190806153044/logo.png?width=128&format=jpg&quality=75"
+              src="https://cdn-assets.affirm.com/vcn_buy/v1/merchants/MNLK8D11U6PV4THN/logo_20190711215210/logo.png?width=128&format=jpg&quality=75"
               className="rounded-full mx-4 my-4 w-20 h-20 drop-shadow-2xl"
             />
           </div>
 
-          <h1 className="my-2">Peloton E-commerce</h1>
+          <h1 className="mt-2">Casper</h1>
+
+          <h1 className="mb-2">As low as 0% APR</h1>
         </div>
 
         <div className="col-span-1 row-span-1 p-4 flex flex-col items-start">
           <div
-            id={styles.roomandboard}
-            className=" w-full h-full pt-48 md:pt-12 ease-in-out rounded-3xl"
+            id={styles.figs}
+            className=" w-full h-full pt-48 md:pt-12 ease-in-out rounded-3xl bg-white"
           >
             <img
-              src="https://cdn-assets.affirm.com/vcn_buy/v1/merchants/NTMW6QA9FVTCHPFW/logo_20190806153044/logo.png?width=128&format=jpg&quality=75"
-              className="rounded-full mx-4 my-4 w-20 h-20 drop-shadow-2xl"
+              src="https://cdn-assets.affirm.com/vcn_buy/v1/merchants/GFFXXAU4GF2951K5/logo/logo.png?width=128&format=jpg&quality=75"
+              className="rounded-full mx-4 my-4 w-20 h-20 drop-shadow-2xl bg-white"
             />
           </div>
 
-          <h1 className="my-2">Peloton E-commerce</h1>
+          <h1 className="mt-2">Figs</h1>
+
+          <h1 className="mb-2">As low as 0% APR</h1>
         </div>
 
         <div className="col-span-1 row-span-1 p-4 flex flex-col items-start">
           <div
-            id={styles.roomandboard}
+            id={styles.goat}
             className=" w-full h-full pt-48 md:pt-12 ease-in-out rounded-3xl"
           >
             <img
-              src="https://cdn-assets.affirm.com/vcn_buy/v1/merchants/NTMW6QA9FVTCHPFW/logo_20190806153044/logo.png?width=128&format=jpg&quality=75"
+              src="https://cdn-assets.affirm.com/vcn_buy/v1/merchants/K7DQ6PUAWD5M6F0Y/logo20191029/logo_offer.png?width=128&format=jpg&quality=75"
               className="rounded-full mx-4 my-4 w-20 h-20 drop-shadow-2xl"
             />
           </div>
 
-          <h1 className="my-2">Peloton E-commerce</h1>
+          <h1 className="mt-2">Goat</h1>
+
+          <h1 className="mb-2">As low as 0% APR</h1>
         </div>
 
         <div className="col-span-1 row-span-1 p-4 flex flex-col items-start">
           <div
-            id={styles.roomandboard}
+            id={styles.dyson}
             className=" w-full h-full pt-48 md:pt-12 ease-in-out rounded-3xl"
           >
             <img
-              src="https://cdn-assets.affirm.com/vcn_buy/v1/merchants/NTMW6QA9FVTCHPFW/logo_20190806153044/logo.png?width=128&format=jpg&quality=75"
+              src="https://cdn-assets.affirm.com/vcn_buy/v1/merchants/7KSWE7M9ANY6F48C/logo20191212/logo_offer.jpg?width=128&format=jpg&quality=75"
               className="rounded-full mx-4 my-4 w-20 h-20 drop-shadow-2xl"
             />
           </div>
 
-          <h1 className="my-2">Peloton E-commerce</h1>
+          <h1 className="mt-2">Dyson</h1>
+
+          <h1 className="mb-2">As low as 0% APR</h1>
         </div>
 
         <div className="col-span-1 row-span-1 p-4 flex flex-col items-start">
           <div
-            id={styles.roomandboard}
+            id={styles.joybird}
             className=" w-full h-full pt-48 md:pt-12 ease-in-out rounded-3xl"
           >
             <img
-              src="https://cdn-assets.affirm.com/vcn_buy/v1/merchants/NTMW6QA9FVTCHPFW/logo_20190806153044/logo.png?width=128&format=jpg&quality=75"
+              src="https://cdn-assets.affirm.com/vcn_buy/v1/merchants/J82E1ZS1P2YHE6S1/logo_20190712104700/logo.png?width=128&format=jpg&quality=75"
               className="rounded-full mx-4 my-4 w-20 h-20 drop-shadow-2xl"
             />
           </div>
 
-          <h1 className="my-2">Peloton E-commerce</h1>
+          <h1 className="mt-2">Joybird</h1>
+
+          <h1 className="mb-2">As low as 0% APR</h1>
         </div>
 
         <div className="col-span-1 row-span-1 p-4 flex flex-col items-start">
           <div
-            id={styles.roomandboard}
+            id={styles.adidas}
             className=" w-full h-full pt-48 md:pt-12 ease-in-out rounded-3xl"
           >
             <img
-              src="https://cdn-assets.affirm.com/vcn_buy/v1/merchants/NTMW6QA9FVTCHPFW/logo_20190806153044/logo.png?width=128&format=jpg&quality=75"
+              src="https://cdn-assets.affirm.com/vcn_buy/v1/merchants/7B6478KO7RT4JTU0/logo2019111307/logo_offer.png?width=128&format=jpg&quality=75"
               className="rounded-full mx-4 my-4 w-20 h-20 drop-shadow-2xl"
             />
           </div>
 
-          <h1 className="my-2">Peloton E-commerce</h1>
+          <h1 className="mt-2">Adidas</h1>
+
+          <h1 className="mb-2">As low as 0% APR</h1>
         </div>
       </div>
     </div>
--- pages/components/Featured.tsx
+++ pages/components/Featured.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from "./css/Feature.module.css";
 
 const Featured = () => {
   return (
-    <div className="px-4 md:px-12 py-24">
+    <div className="px-4 md:px-16 py-24">
       <div className="flex flex-row justify-between items-center py-8">
         <h1 className="text-4xl mr-4 flex flex-col text-left items-start">
           Featured Stores
@@ -12,73 +13,133 @@ const Featured = () => {
         </h1>
       </div>
 
-      <div className="grid md:grid-cols-4 md:grid-rows-2 grid-rows-8 grid-cols-1 gap-4 ">
-        <div className="col-span-1 m-2">
-          <img
-            src="https://cdn-assets.affirm.com/vcn_buy/v1/merchants/ADZ7B0E05VRTMPN2/hero20190109/hero_offer.jpg?width=576px&format=jpg&quality=75"
-            alt="mac"
-            className="object-cover"
-          />
-          <h1 className="pt-2 font-bold">Delta Vacations</h1>
+      <div className="grid md:grid-cols-4 md:grid-rows-2 grid-rows-8 grid-cols-1 gap-1 ">
+        <div className="col-span-1 row-span-1 p-4 flex flex-col items-start">
+          <div
+            id={styles.deltavacations}
+            className=" w-full h-full pt-48 md:pt-12 ease-in-out rounded-3xl"
+          >
+            <img
+              src="https://cdn-assets.affirm.com/vcn_buy/v1/merchants/IIOERFI5KRYG1QR0/logo20191001/logo_offer.png?width=128&format=jpg&quality=75"
+              className="rounded-full mx-4 my-4 w-20 h-20 drop-shadow-2xl bg-white"
+            />
+          </div>
+
+          <h1 className="mt-2">Delta Vacations</h1>
+
+          <h1 className="mb-2 invisible">As low as 0% APR</h1>
         </div>
-        <div className="col-span-1 m-2">
-          <img
-            src="https://cdn-assets.affirm.com/merchant/promos/KGYHY5KUZIA6C5ET/hero2022012002/hero_27.png?width=576px&format=jpg&quality=75"
-            alt="mac"
-            className="object-cover"
-          />
-          <h1 className="pt-2 font-bold">Saks Fifth Avenue</h1>
-          <h1 className="pb-2">As low as 0% APR</h1>
+
+        <div className="col-span-1 row-span-1 p-4 flex flex-col items-start">
+          <div
+            id={styles.saksfifthavenue}
+            className=" w-full h-full pt-48 md:pt-12 ease-in-out rounded-3xl"
+          >
+            <img
+              src="https://cdn-assets.affirm.com/merchant/promos/KGYHY5KUZIA6C5ET/logo2021032946/saks_logo.png?width=128&format=jpg&quality=75"
+              className="rounded-full mx-4 my-4 w-20 h-20 drop-shadow-2xl"
+            />
+          </div>
+
+          <h1 className="mt-2">Simply Mac</h1>
+
+          <h1 className="mb-2">As low as 0% APR</h1>
         </div>
-        <div className="col-span-1 m-2">
-          <img
-            src="https://cdn-assets.affirm.com/vcn_buy/v1/merchants/P2T82BO89LRD4WYH/hero_20190710101735/hero.png?width=576px&format=jpg&quality=75"
-            alt="mac"
-            className="object-cover"
-          />
-          <h1 className="pt-2 font-bold">Reverb</h1>
+
+        <div className="col-span-1 row-span-1 p-4 flex flex-col items-start">
+          <div
+            id={styles.reverb}
+            className=" w-full h-full pt-48 md:pt-12 ease-in-out rounded-3xl"
+          >
+            <img
+              src="https://cdn-assets.affirm.com/vcn_buy/v1/merchants/P2T82BO89LRD4WYH/logo_20190710101744/logo.png?width=128&format=jpg&quality=75"
+              className="rounded-full mx-4 my-4 w-20 h-20 drop-shadow-2xl"
+            />
+          </div>
+
+          <h1 className="mt-2">Simply Mac</h1>
+
+          <h1 className="mb-2">As low as 0% APR</h1>
         </div>
-        <div className="col-span-1 m-2">
-          <img
-            src="https://cdn-assets.affirm.com/vcn_buy/v1/merchants/MZ5NBOKI64F9QBB3/hero_20190806145556/hero.png?width=576px&format=jpg&quality=75"
-            alt="mac"
-            className="object-cover h-42 w-full"
-          />
-          <h1 className="pt-2 font-bold">Mirror</h1>
+
+        <div className="col-span-1 row-span-1 p-4 flex flex-col items-start">
+          <div
+            id={styles.mirror}
+            className=" w-full h-full pt-48 md:pt-12 ease-in-out rounded-3xl"
+          >
+            <img
+              src="https://cdn-assets.affirm.com/vcn_buy/v1/merchants/MZ5NBOKI64F9QBB3/logo_20190806145554/logo.png?width=128&format=jpg&quality=75"
+              className="rounded-full mx-4 my-4 w-20 h-20 drop-shadow-2xl"
+            />
+          </div>
+
+          <h1 className="mt-2">Simply Mac</h1>
+
+          <h1 className="mb-2">As low as 0% APR</h1>
         </div>
-        <div className="col-span-1 m-2">
-          <img
-            src="https://cdn-assets.affirm.com/vcn_buy/v1/merchants/Z0BQ6KB8BLMXH75Q/hero20191204/hero_offer.png?width=576px&format=jpg&quality=75"
-            alt="mac"
-            className="object-cover"
-          />
-          <h1 className="pt-2 font-bold">Expedia Hotels & Vacation Packages</h1>
+
+        <div className="col-span-1 row-span-1 p-4 flex flex-col items-start">
+          <div
+            id={styles.expedia}
+            className=" w-full h-full pt-48 md:pt-12 ease-in-out rounded-3xl"
+          >
+            <img
+              src="https://cdn-assets.affirm.com/vcn_buy/v1/merchants/Z0BQ6KB8BLMXH75Q/logo20191204/logo_offer.png?width=128&format=jpg&quality=75"
+              className="rounded-full mx-4 my-4 w-20 h-20 drop-shadow-2xl"
+            />
+          </div>
+
+          <h1 className="mt-2">Simply Mac</h1>
+
+          <h1 className="mb-2">As low as 0% APR</h1>
         </div>
-        <div className="col-span-1 m-2">
-          <img
-            src="https://cdn-assets.affirm.com/vcn_buy/v1/merchants/0ZV27J8P7X3UJJ0R/hero_20190712134510/hero.png?width=576px&format=jpg&quality=75"
-            alt="mac"
-            className="object-cover"
-          />
-          <h1 className="pt-2 font-bold">Molekule</h1>
-          <h1 className="pb-2">As low as 0% APR</h1>
+
+        <div className="col-span-1 row-span-1 p-4 flex flex-col items-start">
+          <div
+            id={styles.molekule}
+            className=" w-full h-full pt-48 md:pt-12 ease-in-out rounded-3xl"
+          >
+            <img
+              src="https://cdn-assets.affirm.com/vcn_buy/v1/merchants/0ZV27J8P7X3UJJ0R/logo_20190712134504/logo.png?width=128&format=jpg&quality=75"
+              className="rounded-full mx-4 my-4 w-20 h-20 drop-shadow-2xl"
+            />
+          </div>
+
+          <h1 className="mt-2">Simply Mac</h1>
+
+          <h1 className="mb-2">As low as 0% APR</h1>
         </div>
-        <div className="col-span-1 m-2">
-          <img
-            src="https://cdn-assets.affirm.com/vcn_buy/v1/merchants/JN8GO75EH36UNBA4/hero2019111307/hero_offer.png?width=576px&format=jpg&quality=75"
-            alt="mac"
-            className="object-cover"
-          />
-          <h1 className="pt-2 font-bold">ThredUP</h1>
+
+        <div className="col-span-1 row-span-1 p-4 flex flex-col items-start">
+          <div
+            id={styles.thredup}
+            className=" w-full h-full pt-48 md:pt-12 ease-in-out rounded-3xl"
+          >
+            <img
+              src="https://cdn-assets.affirm.com/vcn_buy/v1/merchants/JN8GO75EH36UNBA4/logo2019111307/logo_offer.png?width=128&format=jpg&quality=75"
+              className="rounded-full mx-4 my-4 w-20 h-20 drop-shadow-2xl"
+            />
+          </div>
+
+          <h1 className="mt-2">Simply Mac</h1>
+
+          <h1 className="mb-2">As low as 0% APR</h1>
         </div>
-        <div className="col-span-1 m-2">
-          <img
-            src="https://cdn-assets.affirm.com/merchant/promos/44WXOQ22LJEYREMX/hero2022010410/hero_2.png?width=576px&format=jpg&quality=75"
-            alt="mac"
-            className="object-cover"
-          />
-          <h1 className="pt-2 font-bold">Nike</h1>
-          <h1 className="pb-2">As low as 0% APR</h1>
+
+        <div className="col-span-1 row-span-1 p-4 flex flex-col items-start">
+          <div
+            id={styles.nike}
+            className=" w-full h-full pt-48 md:pt-12 ease-in-out rounded-3xl"
+          >
+            <img
+              src="https://cdn-assets.affirm.com/vcn_buy/v1/merchants/44WXOQ22LJEYREMX/logo20201103/logo_offer.png?width=128&format=jpg&quality=75"
+              className="rounded-full mx-4 my-4 w-20 h-20 drop-shadow-2xl"
+            />
+          </div>
+
+          <h1 className="mt-2">Simply Mac</h1>
+
+          <h1 className="mb-2">As low as 0% APR</h1>
         </div>
       </div>
     </div>
